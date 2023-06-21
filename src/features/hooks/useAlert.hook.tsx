d18c7895d9220c326c0b9e6902e707fc465816f1
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-21 19:07:05 -0300

    feat: useAlert testing tdd

--- src/features/hooks/useAlert.hook.tsx
+++ src/features/hooks/useAlert.hook.tsx
@@ -1,16 +1,17 @@
-
 // Podemos cambiar la implementacion a nuestra propia libreria de alerts, gracias al principio solid de inversion de dependencia
 const useAlert = () => {
-    const dispatchSuccessAlert = (message: string) => {
-    };
+  const dispatchSuccessAlert = (message: string) => {
+    alert("SUCCESS: " + message);
+  };
 
-    const dispatchErrorAlert = (message: string) => {
-    };
+  const dispatchErrorAlert = (message: string) => {
+    alert("ERROR: " + message);
+  };
 
-    return {
-        dispatchSuccessAlert,
-        dispatchErrorAlert
-    };
+  return {
+    dispatchSuccessAlert,
+    dispatchErrorAlert,
+  };
 };
 
 export default useAlert;
