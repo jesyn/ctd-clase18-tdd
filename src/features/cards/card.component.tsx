8631a6e6ed530a843bf6859f0113d5a1dd97b2f8
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-21 19:06:40 -0300

    feat: card, testing tdd

--- src/features/cards/card.component.tsx
+++ src/features/cards/card.component.tsx
@@ -1,14 +1,22 @@
-import {FC} from "react";
-import {Character} from "../../types/character.types";
+import { FC } from "react";
+import { Character } from "../../types/character.types";
 
 export type CardProps = {
-    character: Character
-}
+  character: Character;
+};
 
-const Card:FC<CardProps> = ({character}: CardProps) => {
-   return <div>
-
+const Card: FC<CardProps> = ({ character }: CardProps) => {
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      {character.image && <img src={character.image} alt={character.name} />}
+      {character.followers ? (
+        <p>{character.followers} followers</p>
+      ) : (
+        "No followers yet"
+      )}
     </div>
-}
+  );
+};
 
 export default Card;
